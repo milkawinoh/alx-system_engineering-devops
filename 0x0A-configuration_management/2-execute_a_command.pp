6f7b147b471cdbc4789f1c79d1e8f300a0a74805
milkawinoh <milkawinoh@gmail.com>
2023-11-24 16:35:34 +0300

    updated

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
-exec{ 'killmenow':
-command => 'pkill -f killmenow',
-path => '/bin:/usr/bin',
-onlyif => 'pgrep -f killmenow'
-
+# Executes a bash command
+exec { 'killmenow':
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin']
 }
